02f6ffc025a6ec17973b4506486b7fd61116d5d8
RatiborM <r_morkvin@vk.com>
2024-10-25 23:14:05 +0300

    выполнил третий этап

---
+++ brain_games/cli.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import prompt
+
+
+def welcome_user():
+    """Спрашиваем имя пользователя и приветствуем его."""
+    name = prompt.string('May I have your name? ')
+    print('Hello, {0}!'.format(name))
